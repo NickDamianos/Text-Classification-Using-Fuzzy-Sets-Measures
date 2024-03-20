eba53bda9edd615cfab89f922b579bbfe3e0bf9d
NickDamianos <nikolaos_damianos_1907@hotmail.com>
2024-03-20 19:35:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+@author: nikolaos damianos
+#######################################################################################################################
 from sklearn.feature_extraction.text import CountVectorizer
 #from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.feature_selection import mutual_info_classif
